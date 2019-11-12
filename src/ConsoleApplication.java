287cafaa928760d6cc5051030d420b309306baf9
Ayaka Tsubouchi <ayakawell@gmail.com>
2019-11-12 09:44:18 -0800

    first commit

---
+++ src/ConsoleApplication.java
@@ -0,0 +1,12 @@
+
+public class ConsoleApplication {
+	
+
+	public static void main(String[] commandLineArguments) {
+		if(commandLineArguments.length < 0) {
+			System.out.println(commandLineArguments.length);
+		}
+
+	}
+
+}
